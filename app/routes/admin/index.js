21f9906b74a9d15adc97ff86b717f44d263e5d5c
jsamuel89 <mailtosamuelj@gmail.com>
2018-03-12 08:30:36 +0530

    admin login created

--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -4,7 +4,7 @@ import groupBy from 'ember-group-by';
 
 
 export default Route.extend({
- 
+
   model() {
 
     var thisObj = this;
@@ -13,106 +13,51 @@ export default Route.extend({
       all: Ember.computed(function () {
           return thisObj.store.peekAll('gift');
         })
-
         ,
       qview: Ember.computed('all', function () {
           return this.get('all');
         })
-
-        //   , queued : Ember.computed.filterBy('qview', 'queued', true) //.filterBy('yettoarrive',false) // .sort Timeorder.
-
-        //   // This should be waiting.
-        //   , arrived : Ember.computed.filterBy('queued', 'yettoarrive', false)
-
-
-        //   , uniquedeptartments : Ember.computed.uniqBy('all','deptname')
-
-        //   , deptartments : Ember.computed.map('uniquedeptartments',function(deptartment) {
-        //       return {title : deptartment.get('deptname') }
-        //   })
-
-        //   , uniquedoctors : Ember.computed.uniqBy('qview','proname') 
-
-        //   , doctors : Ember.computed.map('uniquedoctors',function(doctor) {
-        //       return {title : doctor.get('proname') }
-        //   })
-
-        //   , selecteddepartments : []
-
-        //   , filtereddepartments : Ember.computed('selecteddepartments.[]' , function() {
-
-        //     console.log(this.get('qview'))
-        //     if(this.get('selecteddepartments').length > 0) {
-        //       return this.get('qview').filter( (element) => {            
-        //         if((this.get('selecteddepartments').indexOf(element.get('deptname'))) > -1) {
-        //           return element;
-        //         }            
-        //       })
-
-        //     }
-        //     else {
-        //       return this.get('all')
-        //     }
-
-        //   })
-
-        //   , selecteddoctors : []
-
-        //   , filtereddoctors : Ember.computed('selecteddoctors.[]' , function() {
-        //       console.log(this.get('qview'))        
-        //       if(this.get('selecteddoctors').length > 0) {
-
-        //         return this.get('qview').filter( (element) => {        
-        //           if((this.get('selecteddoctors').indexOf(element.get('proname'))) > -1) {
-        //             return element;
-        //           }            
-        //         })
-
-        //       }
-        //       else {
-        //         return this.get('all')
-        //       }
-
-        //   })
-
         ,
       giftsByGroups: groupBy('qview', 'type')
 
-
-      // , waiting : Ember.computed.filterBy('queueByDoctors','visitstatus','waiting')
-
-      //   , calling : Ember.computed.filterBy('queueByDoctors','visitstatus','calling')
-
     }).create();
-
-
-    // var visitStates = ['waiting', 'calling', 'serving', 'consumed'];
-    // visitStates.forEach(function(element) {
-    //   Ember.defineProperty(this, element, Ember.computed.filterBy('qview', 'visitstatus', element))
-    // }, this.gifts);
-
-
 
     return this.store.findAll('gift').then((gifts) => {
 
-      //   Ember.set(this.gifts, 'qviewfilter', 'all');
-
       return Ember.RSVP.hash({
+        
         gifts: this.gifts.get('all'),
         qi: this.gifts,
-        newgift : this.store.createRecord('gift')
+        newgift: this.store.createRecord('gift')
+        ,isValidated : true
+        
       })
     })
 
   }
-  , actions : {
+	, setupController: function(controller,model) {
+    this._super(controller, model);
+    
+  }
+  ,actions: {
     addnewgift(newgift) {
       // var self = this;
-      newgift.save().then( () => {
-        this.controller.set('model.newgift',this.store.createRecord('gift'))
+      newgift.save().then(() => {
+        this.controller.set('model.newgift', this.store.createRecord('gift'))
         // self.set('newgift','');
       })
+
+    }
     
+    , authenticate (username,password) {
+      var modelObject = this.modelFor(this.routeName);
+      if(username === '1' && password === '1') { 
+        Ember.set(modelObject, "isValidated", true);
+        Materialize.toast('Login success', 4000 , 'green rounded') // 4000 is the duration of the toast
+
+      } else {
+        Materialize.toast('Invalid credentials', 4000 , 'red rounded') // 4000 is the duration of the toast
+      }
     }
   }
 });
